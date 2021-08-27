998272dc44e745a8a734eb13dbbe7f3359264069
Chris Reichert <chris.a.reichert@gmail.com>
2021-08-26 23:01:32 -0500

    attempted to make the cart clear to work

--- js/cart.js
+++ js/cart.js
@@ -9,7 +9,7 @@ let cart;
 function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
   cart = new Cart(cartItems);
-  console.log("hi" , cartItems)
+  console.log("hi", cartItems)
   showCart(cartItems.items);
 }
 
@@ -23,38 +23,49 @@ function renderCart() {
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
+  let tableSelect = document.querySelector('tbody')
+  let removeRow = document.getElementsByClassName('cartLine');
 
+  
+  tableSelect.removeChild(removeRow);
+
+  // tr.innerHTML = 'empty string';
 }
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart(items) {
   let cartContentsTable = document.querySelector('tbody')
-// DONE: Find the table body
-for (let i = 0 ; i < items.length; i ++){
-  let tr = document.createElement('tr')
-  // tr.textContent = items[i].items
-  cartContentsTable.appendChild(tr)
-  tr.textContent = [i].name
-  let td1 = document.createElement('td')
-  tr.appendChild(td1)
-  let a = document.createElement('a')
-  let linktext = document.createTextNode('X');
-  a.setAttribute("href", 'https://www.google.com/?client=safari');
-  td1.appendChild(a);
-  a.appendChild(linktext);
-  // a.textContent = 'remove'
-  // td1.textContent = 'X'
-  let td2 = document.createElement('td')
-  tr.appendChild(td2)
-  td2.textContent = items[i].quantity
-  let td3 = document.createElement('td')
-  tr.appendChild(td3)
-  td3.textContent = items[i].items
-}
+  // DONE: Find the table body
+  for (let i = 0; i < items.length; i++) {
+    let tr = document.createElement('tr')
+    // tr.textContent = items[i].items
+    cartContentsTable.appendChild(tr)
+    tr.textContent = [i].name
+    tr.setAttribute('class', 'cartLine');
+    let td1 = document.createElement('td')
+    tr.appendChild(td1)
+    let a = document.createElement('a')
+    let linktext = document.createTextNode('X');
+    a.setAttribute("href", 'https://www.google.com/?client=safari');
+    td1.appendChild(a);
+    a.appendChild(linktext);
+    // a.textContent = 'remove'
+    // td1.textContent = 'X'
+    let td2 = document.createElement('td')
+    tr.appendChild(td2)
+    td2.textContent = items[i].quantity
+    let td3 = document.createElement('td')
+    tr.appendChild(td3)
+    td3.textContent = items[i].items
+  }
+  let clearCartButton = document.getElementById('cart-container');
+  let cartButton = document.createElement('button')
+  cartButton.textContent ='Clear Cart';
+  clearCartButton.appendChild(cartButton);
   // DONE: Iterate over the items in the cart
   // DONE: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  // DONE: Create a TD for the delete link, quantity,  and the item
+  // DONE: Add the TR to the TBODY and each of the TD's to the TR
 
 }
 
